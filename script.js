5c0520ff0305cbbd4b2b78135c13b8519c93674a
Ronaldo Cerenza <rockronald@gmail.com>
2023-02-07 14:37:22 -0400

    Enviando informações para localStorage

--- script.js
+++ script.js
@@ -4,13 +4,13 @@
 const paletas = document.querySelectorAll('#color-palette .color');
 
 //coloquei as cores manualmente pra passar no requisito 2
+let colors = ['black', 'red', 'blue', 'green'];
 
-paletas[0].style.backgroundColor = 'black';
-paletas[1].style.backgroundColor = 'red';
-paletas[2].style.backgroundColor = 'blue';
-paletas[3].style.backgroundColor = 'green';
+for (let index = 0; index < paletas.length; index += 1) {
+    paletas[index].style.backgroundColor = colors[index];
+}
 
-//Função que gera as cores
+//Função que gera as cores aleatorias
 
 function generateColor() {
   const letters = '0123456789ABCDEF';
@@ -23,15 +23,30 @@ function generateColor() {
 
 // 4 - Adicione um botão para gerar cores aleatórias para a paleta de cores
 
-button = document.querySelector('#button-random-color');
+const button = document.querySelector('#button-random-color');
 
-button.addEventListener('click', () => {
+//ao clicar no botão as cores serão aleatorias
+
+const clickButton = () => {
   for (let index = 1; index < paletas.length; index += 1) {
-    paletas[0].style.backgroundColor = 'black'
     paletas[index].style.backgroundColor = generateColor();
   }
-});
+  saveStorage() //salva as informações no localStorage
+}
+
+button.addEventListener('click', clickButton);
 
 // 5 - Implemente uma função usando localStorage para que a paleta de cores gerada aleatoriamente seja mantida após recarregar a página
+
+const saveStorage = () => {
+  const colorPalette = {
+    0: paletas[0].style.backgroundColor,
+    1: paletas[1].style.backgroundColor,
+    2: paletas[2].style.backgroundColor,
+    3: paletas[3].style.backgroundColor,
+  };
+  localStorage.setItem('colorPalette', JSON.stringify(colorPalette));
+};
+
 
 
